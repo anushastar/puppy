3d7ba7e0c142308eee9cff33ab92a5b6b681dbf3
Anusha <nitinmathur@Nitins-MBP.attlocal.net>
2024-12-30 11:09:10 -0800

    kjhdkjsd

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/anushastar/puppy
 
 go 1.21.6
+
+require github.com/anushastar/dog v0.0.0-20241229074742-d2221d1562b6 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,20 @@
 package puppy
 
+import "github.com/anushastar/dog"
+import fmt
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof woof woof!!!"
+}
+
+func Bigbark() string {
+	return dog.Grownup(Bark())
+}
+
+func t1() {
+	fmt.Printf("abc")
 }
